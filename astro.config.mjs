c446aa3bbf48853ed9879d8ce8bd77897a201828
ChristianBaby <204805@unsaac.edu.pe>
2026-07-20 22:45:46 -0500

    lenguajes

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,4 +5,11 @@ import sitemap from '@astrojs/sitemap';
 export default defineConfig({
   integrations: [tailwind(), sitemap()],
   site: 'https://www.perubeyond.com',
+  i18n: {
+    defaultLocale: 'es',
+    locales: ['es', 'en', 'pt'],
+    routing: {
+      prefixDefaultLocale: true,
+    },
+  },
 });
